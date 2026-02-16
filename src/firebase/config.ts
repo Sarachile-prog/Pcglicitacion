e293fdefd4261022e26e94c20b0520525f3ed64e
Paulo Córdova <pauloandrescordova@gmail.com>
2026-02-16 23:55:59 +0000

    Estimado cliente:
    Detectamos que se puede acceder públicamente a una cl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-4126028826-31b2f",
-  "appId": "1:114857941103:web:335187773ad7cb6745559a",
-  "apiKey": "AIzaSyD2fJXnTHH7kvVjIhSeUb4HcmHYuGhbxOI",
-  "authDomain": "studio-4126028826-31b2f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "114857941103"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  measurementId: "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || ""
 };

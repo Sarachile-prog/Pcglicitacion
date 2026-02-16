e8e741e1e81be89a246cf8d3ce615d4ee4ef00e3
Paulo Córdova <pauloandrescordova@gmail.com>
2026-02-16 23:58:03 +0000

    dame en detalle paso a paso las acciones que debo hacer en google cloud,

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-4126028826-31b2f",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:4126028826:web:7ed7365160906ba031b2f3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD2fJXnTHH7kvVjIhSeUb4HcmHYuGhbxOI",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-4126028826-31b2f.firebaseapp.com",
   measurementId: "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || ""
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "4126028826"
 };
